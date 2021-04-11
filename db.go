eb1dd01ba3ad7361bc51f5c2d488763f1d6ca819
Sujit <itsursujit@gmail.com>
2021-04-11 13:52:50 +0545

    [wip]

--- db.go
+++ db.go
@@ -1,7 +1,9 @@
 package db
 
 import (
+	"errors"
 	"fmt"
+	"math"
 	"time"
 
 	"github.com/sujit-baniya/log"
@@ -14,12 +16,12 @@ import (
 )
 
 type Config struct {
-	Driver      string `yaml:"driver" env:"DB_DRIVER"`
-	Host        string `yaml:"host" env:"DB_HOST"`
-	Username    string `yaml:"username" env:"DB_USER"`
-	Password    string `yaml:"password" env:"DB_PASS"`
-	DBName      string `yaml:"db_name" env:"DB_NAME"`
-	Port        int    `yaml:"port" env:"DB_PORT"`
+	Driver     string `yaml:"driver" env:"DB_DRIVER"`
+	Host       string `yaml:"host" env:"DB_HOST"`
+	Username   string `yaml:"username" env:"DB_USER"`
+	Password   string `yaml:"password" env:"DB_PASS"`
+	DBName     string `yaml:"db_name" env:"DB_NAME"`
+	Port       int    `yaml:"port" env:"DB_PORT"`
 	MaxOpenCon int    `yaml:"connections" env:"DB_CONNECTIONS"`
 	MaxIdleCon int    `yaml:"idle_connections" env:"DB_IDLE_CONNECTIONS"`
 }
@@ -70,4 +72,111 @@ func New(cfg Config) (*gorm.DB, error) {
 			SetMaxOpenConns(100),
 	)
 	return db, nil //nolint:wsl
+}
+
+type Pagination struct {
+	TotalRecords int64 `json:"total_records" query:"total_records" form:"total_records"`
+	TotalPage    int   `json:"total_page" query:"total_page" form:"total_page"`
+	Offset       int   `json:"offset" query:"offset" form:"offset"`
+	Limit        int   `json:"limit" query:"limit" form:"limit"`
+	Page         int   `json:"page" query:"page" form:"page"`
+	PrevPage     int   `json:"prev_page" query:"prev_page" form:"prev_page"`
+	NextPage     int   `json:"next_page" query:"" form:""`
+}
+
+type Paging struct {
+	Page    int      `json:"page" query:"page" form:"page"`
+	OrderBy []string `json:"order_by" query:"order_by" form:"order_by"`
+	Limit   int      `json:"limit" query:"limit" form:"limit"`
+	ShowSQL bool
+}
+
+type Param struct {
+	DB     *gorm.DB
+	Paging *Paging
+}
+
+// Endpoint for pagination
+func Pages(p *Param, result interface{}) (paginator *Pagination, err error) {
+
+	var (
+		done     = make(chan bool, 1)
+		db       = p.DB.Session(&gorm.Session{})
+		defPage  = 1
+		defLimit = 20
+		count    int64
+		offset   int
+	)
+
+	// get all counts
+	go getCounts(db, result, done, &count)
+
+	// if not defined
+	if p.Paging == nil {
+		p.Paging = &Paging{}
+	}
+
+	// debug sql
+	if p.Paging.ShowSQL {
+		db = db.Debug()
+	}
+	// limit
+	if p.Paging.Limit == 0 {
+		p.Paging.Limit = defLimit
+	}
+	// page
+	if p.Paging.Page < 1 {
+		p.Paging.Page = defPage
+	} else if p.Paging.Page > 1 {
+		offset = (p.Paging.Page - 1) * p.Paging.Limit
+	}
+	// sort
+	if len(p.Paging.OrderBy) > 0 {
+		for _, o := range p.Paging.OrderBy {
+			db = db.Order(o)
+		}
+	} else {
+		str := "id desc"
+		p.Paging.OrderBy = append(p.Paging.OrderBy, str)
+	}
+
+	// get
+	if errGet := db.Limit(p.Paging.Limit).Offset(offset).Find(result).Error; errGet != nil && !errors.Is(errGet, gorm.ErrRecordNotFound) {
+		return nil, errGet
+	}
+	<-done
+
+	// total pages
+	total := int(math.Ceil(float64(count) / float64(p.Paging.Limit)))
+
+	// construct pagination
+	paginator = &Pagination{
+		TotalRecords: count,
+		Page:         p.Paging.Page,
+		Offset:       offset,
+		Limit:        p.Paging.Limit,
+		TotalPage:    total,
+		PrevPage:     p.Paging.Page,
+		NextPage:     p.Paging.Page,
+	}
+
+	// prev page
+	if p.Paging.Page > 1 {
+		paginator.PrevPage = p.Paging.Page - 1
+	}
+	// next page
+	if p.Paging.Page != paginator.TotalPage {
+		paginator.NextPage = p.Paging.Page + 1
+	}
+
+	return paginator, nil
+}
+
+func getCounts(db *gorm.DB, anyType interface{}, done chan bool, count *int64) {
+	db.Model(anyType).Count(count)
+	done <- true
+}
+
+func (p Pagination) IsEmpty() bool {
+	return p.TotalRecords <= 0
 }
--- go.mod
+++ go.mod
@@ -3,9 +3,14 @@ module github.com/sujit-baniya/db
 go 1.16
 
 require (
-	github.com/sujit-baniya/log v1.0.62
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/jackc/pgproto3/v2 v2.0.7 // indirect
+	github.com/jackc/pgx/v4 v4.11.0 // indirect
+	github.com/sujit-baniya/log v1.0.63
+	golang.org/x/text v0.3.6 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	gorm.io/driver/mysql v1.0.5
 	gorm.io/driver/postgres v1.0.8
-	gorm.io/gorm v1.21.6
+	gorm.io/gorm v1.21.7
 	gorm.io/plugin/dbresolver v1.1.0
 )
